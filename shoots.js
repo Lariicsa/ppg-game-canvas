14ccdf998d49c153409f48afcbddc2aeabb175ee
Larissa <dejamepienso@gmail.com>
2019-08-27 13:07:20 -0500

    Now Buttercup shoots in  rainbow laser

--- shoots.js
+++ shoots.js
@@ -1,6 +1,6 @@
-var canvas = document.getElementById('canvas');
+let canvas = document.getElementById('canvas');
 
-var context = canvas.getContext('2d');
+let context = canvas.getContext('2d');
 
 class Board {
   constructor() {
@@ -50,44 +50,72 @@ class makeSquare{
     this.s= speed
     this.img = new Image()
     this.img.src = './images/buttercup1.png'
-    this.img2 = new Image()
-    this.img2.src = './images/buttercup2.png'
+    this.imgl = new Image()
+    this.imgl.src = './images/power.png'
   }
 
   draw() {
-    //context.fillRect(this.x, this.y, this.l, this.l, this.img)
     context.drawImage(this.img, this.x, this.y, this.l, this.l)
   }
 }
 
-//Background
-const board = new Board()
 
-const ship = new makeSquare(50, canvas.height / 2 - 25, 50, 5)
+class Enemy{
+  constructor(x, y, length, speed) {
+    this.x= x
+    this.y= y
+    this.l= length
+    this.s= speed
+    this.imgv1 = new Image()
+    this.imgv1.src = './images/gruber.png'
+  }
 
+  draw() {
+    context.drawImage(this.imgv1, this.x, this.y, this.l, this.l)
+  }
+}
 
+let enemies = [];
 
-var up = false;
-var down = false;
-var space = false;
-
-
-var shooting = false;
-
-const bullet = new makeSquare(0, 0, 10, 10)
-
-
-var enemies = [];
-
-
-var enemyBaseSpeed = 2;
+let enemyBaseSpeed = 2;
 function makeEnemy() {
   var enemyX = canvas.width;
   var enemySize = Math.round((Math.random() * 45)) + 15;
   var enemyY = Math.round(Math.random() * (canvas.height - enemySize * 2)) + enemySize;
   var enemySpeed = Math.round(Math.random() * enemyBaseSpeed) + enemyBaseSpeed;
-  enemies.push(new makeSquare(enemyX, enemyY, enemySize, enemySpeed));
+  enemies.push(new Enemy(enemyX, enemyY, enemySize, enemySpeed));
 }
+
+//Background
+const board = new Board()
+
+//girl
+const ship = new makeSquare(50, canvas.height / 2 - 25, 50, 5)
+
+let up = false;
+let down = false;
+let space = false;
+let shooting = false;
+
+class Laser{
+  constructor(x, y, length, height, speed) {
+    this.x= x
+    this.y= y
+    this.l= length
+    this.height = height
+    this.s= speed
+    this.imgpow = new Image()
+    this.imgpow.src = './images/power.png'
+  }
+
+  draw() {
+    context.drawImage(this.imgpow, this.x, this.y, this.l, this.height)
+  }
+}
+
+const bullet = new Laser(0, 0, 100, 14, 10)
+
+console.log(bullet)
 
 
 function isWithin(a, b, c) {
@@ -96,7 +124,7 @@ function isWithin(a, b, c) {
 
 
 function isColliding(a, b) {
-  var result = false;
+  let result = false;
   if (isWithin(a.x, b.x, b.x + b.l) || isWithin(a.x + a.l, b.x, b.x + b.l)) {
     if (isWithin(a.y, b.y, b.y + b.l) || isWithin(a.y + a.l, b.y, b.y + b.l)) {
       result = true;
@@ -106,23 +134,25 @@ function isColliding(a, b) {
 }
 
 
-var score = 0;
+let score = 0;
 
-var timeBetweenEnemies = 5 * 1000;
+let timeBetweenEnemies = 5 * 1000;
 
-var timeoutId = null;
+let timeoutId = null;
 
 
 function startGame() {
-  board.draw()
-
+  
 	// Kick off the enemy spawn interval
   timeoutId = setInterval(makeEnemy, timeBetweenEnemies);
+
+  board.draw()
   // Make the first enemy
+  
   setTimeout(makeEnemy, 1000);
-  // Kick off the draw loop
+
   draw();
-  // Stop listening for click events
+  
   canvas.removeEventListener('click', startGame);
 }
 
@@ -134,9 +164,11 @@ function endGame() {
   
   erase();
   context.font = '50px Sonsie One'
- 
-  //context.fillText('Game Over', canvas.width / 2 - 140, 200)
+
+  board.draw()
+  context.fillStyle = '#213867';
   context.fillText('Game Over. Final Score: ' + score, canvas.width / 2, canvas.height / 2);
+  
 }
 
 
@@ -166,7 +198,6 @@ canvas.addEventListener('keyup', function(event) {
 
 
 function erase() {
-  
   context.fillRect(0, 0, 800, 400);
 }
 
@@ -187,16 +218,18 @@ function draw() {
   var gameOver = false;
 
   enemies.forEach(function(enemy) {
+    enemy.draw() //added
     enemy.x -= enemy.s;
     if (enemy.x < 0) {
       gameOver = true;
     }
-    context.fillStyle = '#00FF00';
-    enemy.draw();
+    context.fillStyle = '#111';
+    
   });
 
   enemies.forEach(function(enemy, i) {
     if (isColliding(enemy, ship)) {
+      enemy.draw()  //added
       gameOver = true;
     }
   });
@@ -222,6 +255,7 @@ function draw() {
     bullet.x += bullet.s;
 
     enemies.forEach(function(enemy, i) {
+      enemy.draw() //added
       if (isColliding(bullet, enemy)) {
         enemies.splice(i, 1);
         score++;
@@ -241,7 +275,7 @@ function draw() {
       shooting = false;
     }
   
-    context.fillStyle = '#0000FF';
+    context.fillStyle = '#09f';
     bullet.draw();
   }
  
