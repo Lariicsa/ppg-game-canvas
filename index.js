55b9c60536b5a9f77a28cf95a278c8e0154de1fd
Larissa <larissa@fantasticocomic.com>
2022-01-22 16:55:38 -0600

    Refactor to ES6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import Board from "./modules/board.js";
 import Powergirl from "./modules/Powergirl.js";
 import Enemy from "./modules/Enemy.js";
-import Laser from './modules/Laser.js'
+import Laser from "./modules/Laser.js";
 
 let CANVAS = document.getElementById("canvas");
 let CONTEXT = CANVAS.getContext("2d");
@@ -23,28 +23,28 @@ let girl2 = document.querySelector("#g2");
 let girl3 = document.querySelector("#g3");
 let girl;
 let startScreen = document.querySelector(".choose");
-
 let enemies = [];
 let enemyBaseSpeed = 2;
-function makeEnemy() {
+
+const makeEnemy = () => {
   let enemyX = CANVAS.width;
   let enemySize = Math.round(Math.random() * 100) + 45;
   let enemyY =
     Math.round(Math.random() * (CANVAS.height - enemySize * 2)) + enemySize;
   let enemySpeed = Math.round(Math.random() * enemyBaseSpeed) + enemyBaseSpeed;
   enemies.push(new Enemy(enemyX, enemyY, enemySize, enemySpeed, CONTEXT));
-}
+};
 
 //Background
 const board = new Board(CANVAS.width, CANVAS.height, CONTEXT);
 //laser
 const bullet = new Laser(0, 0, 100, 14, 10, CONTEXT);
 
-function isWithin(a, b, c) {
+const isWithin = (a, b, c) => {
   return a > b && a < c;
-}
+};
 
-function isColliding(a, b) {
+const isColliding = (a, b) => {
   let result = false;
   if (isWithin(a.x, b.x, b.x + b.l) || isWithin(a.x + a.l, b.x, b.x + b.l)) {
     if (isWithin(a.y, b.y, b.y + b.l) || isWithin(a.y + a.l, b.y, b.y + b.l)) {
@@ -52,17 +52,17 @@ function isColliding(a, b) {
     }
   }
   return result;
-}
+};
 
-function startGame() {
+const startGame = () => {
   CANVAS.focus();
   timeoutId = setInterval(makeEnemy, timeBetweenEnemies, timeBetweenCandies);
   board.draw();
   setTimeout(makeEnemy, 1000);
   draw();
-}
+};
 
-function endGame() {
+const endGame = () => {
   board.audio2.play();
   board.draw();
   clearInterval(timeoutId);
@@ -72,9 +72,9 @@ function endGame() {
   CONTEXT.fillStyle = "#213867";
   CONTEXT.fillText("Game Over.", CANVAS.width / 2 - 200, CANVAS.height / 2);
   scoreHearts(score);
-}
+};
 
-function scoreHearts(score) {
+const scoreHearts = (score) => {
   board.audio.currentTime = 0;
   board.audio.pause();
   end.style.display = "flex";
@@ -85,9 +85,9 @@ function scoreHearts(score) {
     girlAngry.style.display = "flex";
     end.classList.add("fail");
   }
-}
+};
 
-CANVAS.addEventListener("keydown", function (event) {
+CANVAS.addEventListener("keydown", (event) => {
   event.preventDefault();
   if (event.keyCode === 38) {
     // UP
@@ -107,7 +107,7 @@ CANVAS.addEventListener("keydown", function (event) {
   }
 });
 
-CANVAS.addEventListener("keyup", function (event) {
+CANVAS.addEventListener("keyup", (event) => {
   event.preventDefault();
   if (event.keyCode === 38) {
     // UP
@@ -119,41 +119,41 @@ CANVAS.addEventListener("keyup", function (event) {
   }
 });
 
-function erase() {
+const erase = () => {
   CONTEXT.fillRect(0, 0, 800, 400);
-}
+};
 
-function shoot() {
+const shoot = () => {
   bullet.audio.play();
   if (!shooting) {
     shooting = true;
     bullet.x = girl.x + girl.l;
     bullet.y = girl.y + girl.height / 2;
   }
-}
+};
 
-girl1.addEventListener("click", function (e) {
+girl1.addEventListener("click", (e) => {
   girl = new Powergirl(50, CANVAS.height / 2, 150, 68, 5, 0, CONTEXT);
   startScreen.className += " hide";
 });
 
-girl2.addEventListener("click", function (e) {
+girl2.addEventListener("click", (e) => {
   girl = new Powergirl(50, CANVAS.height / 2, 150, 68, 5, 1, CONTEXT);
   startScreen.className += " hide";
 });
 
-girl3.addEventListener("click", function (e) {
+girl3.addEventListener("click", (e) => {
   girl = new Powergirl(50, CANVAS.height / 2, 150, 68, 5, 2, CONTEXT);
   startScreen.className += " hide";
 });
 
 //Draw all
-function draw() {
+const draw = () => {
   erase();
   board.draw();
   let gameOver = false;
 
-  enemies.forEach(function (enemy) {
+  enemies.forEach((enemy) => {
     enemy.draw();
     enemy.x -= enemy.s;
     if (enemy.x < 0) {
@@ -164,7 +164,7 @@ function draw() {
     CONTEXT.fillStyle = "#111";
   });
 
-  enemies.forEach(function (enemy) {
+  enemies.forEach((enemy) => {
     if (isColliding(enemy, girl)) {
       enemy.draw();
 
@@ -193,7 +193,7 @@ function draw() {
   if (shooting) {
     bullet.x += bullet.s;
 
-    enemies.forEach(function (enemy, i) {
+    enemies.forEach((enemy, i) => {
       enemy.draw();
       if (isColliding(bullet, enemy)) {
         enemies.splice(i, 1);
@@ -230,4 +230,4 @@ function draw() {
   } else {
     window.requestAnimationFrame(draw);
   }
-}
+};
